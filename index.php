011680eb2498132f00a61bde1ab0ba4f6f3ced46
kilocharlie96 <cernak.kiko@gmail.com>
2024-07-04 23:13:08 +0200

    Project structure changed to php

---
+++ index.php
@@ -0,0 +1,135 @@
+<?php include 'partials\sections\header.php'; ?>
+
+    <div class="app p-3 is-size-7">
+
+        <h1 class="m-4">STATION FLIGHT REPORT</h1>
+        <header>
+            <ul class="flt-info">
+                <li>
+                    <label for="flt.no.">FLT. NO:</label>
+                    <input type="text" name="flt.no." id="flt.no.">
+                </li>
+                <li>
+                    <label for="date">DATE:</label>
+                    <input type="text" name="date" id="date">
+                </li>
+                <li>
+                    <label for="ac-reg.">A/C REG.:</label>
+                    <input type="text" name="ac-reg." id="ac-reg.">
+                </li>
+                <li>
+                    <label for="ac-type">A/C TYPE:</label>
+                    <input type="text" name="ac-type" id="ac-type">
+                </li>
+                <li>
+                    <label for="stand">STAND:</label>
+                    <input type="text" name="stand" id="stand">
+                </li>
+                <li>
+                    <label for="gate">GATE:</label>
+                    <input type="text" name="gate" id="gate">
+                </li>
+            </ul>
+
+            <div class="arr-dep">
+                <ul>
+                    <li>
+                        <label for="arr">ARRIVAL</label>
+                        <input type="text" name="arr" id="arr">
+                    </li>
+                    <li>
+                        <label for="">STA</label>
+                        <input type="text" name="sta" id="sta">
+                    </li>
+                    <li>
+                        <label for="">ATA</label>
+                        <input type="text" name="ata" id="ata">
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <label for="">PAX</label>
+                        <input type="text" name="" id="">
+                    </li>
+                    <li>
+                        <label for="">PRM</label>
+                        <input type="text" name="" id="">
+                    </li>
+
+                    <li>
+                        <label for="">BAGS</label>
+                        <input type="text" name="" id="">
+                    </li>
+
+                    <li>
+                        <label for="">CGO</label>
+                        <input type="text" name="" id="">
+                    </li>
+
+                    <li>
+                        <label for="">MAIL</label>
+                        <input type="text" name="" id="">
+                    </li>
+
+                    <li>
+                        <label for="">OTHER</label>
+                        <input type="text" name="" id="">
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <label for="arr">DEPARTURE</label>
+                        <input type="text" name="arr" id="arr">
+                    </li>
+                    <li>
+                        <label for="">STD</label>
+                        <input type="text" name="sta" id="sta">
+                    </li>
+                    <li>
+                        <label for="">ATD</label>
+                        <input type="text" name="ata" id="ata">
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <label for="">PAX</label>
+                        <input type="text" name="" id="">
+                    </li>
+                    <li>
+                        <label for="">PRM</label>
+                        <input type="text" name="" id="">
+                    </li>
+
+                    <li>
+                        <label for="">BAGS</label>
+                        <input type="text" name="" id="">
+                    </li>
+
+                    <li>
+                        <label for="">CGO</label>
+                        <input type="text" name="" id="">
+                    </li>
+
+                    <li>
+                        <label for="">MAIL</label>
+                        <input type="text" name="" id="">
+                    </li>
+
+                    <li>
+                        <label for="">OTHER</label>
+                        <input type="text" name="" id="">
+                    </li>
+                </ul>
+                
+        </header>
+
+        <main>
+
+        </main>
+    </div>
+
+</body>
+</html>
---
+++ partials/sections/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Station Flight Report</title>
+    <link rel="stylesheet" href="assets\css\index.css">
+    <link rel="stylesheet" href="assets\css\bulma\css\bulma.css">
+    <link rel="stylesheet" href="assets\css\bulma\css\bulma.css.map">
+    <link rel="stylesheet" href="assets\css\bulma\css\bulma.min.css">
+</head>
+
+<body>

c8ac15df6ddf182ab046d2f9fd27c2685464106a
kilocharlie96 <cernak.kiko@gmail.com>
2024-07-18 12:42:01 +0200

    Fuel figures remade as table.

--- partials/sections/fuel-figures.php
+++ partials/sections/fuel-figures.php
@@ -1,39 +1,86 @@
 <h2 class="subtitle mt-6">FUEL FIGURES</h2>
-<ul class="grid">
-        <li>
-            <label for="zfw">ZFW:</label>
-            <input class="input is-small" type="text" name="zfw" id="zfw">
-        </li>
-        <li>
-            <label for="mtow">MTOW:</label>
-            <input class="input is-small" type="text" name="mtow" id="mtow">
-        </li>
-        <li>
-            <label for="block">BLOCK:</label>
-            <input class="input is-small" type="text" name="block" id="block">
-        </li>
-        <li>
-            <label for="taxi">TAXI:</label>
-            <input class="input is-small" type="text" name="taxi" id="taxi">
-        </li>
-        <li>
-            <label for="trip">TRIP:</label>
-            <input class="input is-small" type="text" name="trip" id="trip">
-        </li>
-        <li>
-            <label for="eet">EET:</label>
-            <input class="input is-small" type="text" name="eet" id="eet">
-        </li>
-        <li>
-            <label for="crew">CREW:</label>
-            <input class="input is-small" type="text" name="crew" id="crew">
-        </li>
-        <li>
-            <label for="dow">DOW:</label>
-            <input class="input is-small" type="text" name="dow" id="dow">
-        </li>
-        <li>
-            <label for="doi">DOI:</label>
-            <input class="input is-small" type="text" name="doi" id="doi">
-        </li>
-    </ul>
+<table class="table is-striped is-bordered">
+    <tbody>
+
+        <tr>
+            <th>
+                <label for="zfw">ZFW</label>
+            </th>
+            <td>
+                <input class="input is-small" type="text" name="zfw" id="zfw">
+            </td>
+        </tr>
+
+        <tr>
+            <th>
+                <label for="mtow">MTOW</label>
+            </th>
+            <td>
+                <input class="input is-small" type="text" name="mtow" id="mtow">
+            </td>
+        </tr>
+
+        <tr>
+            <th>
+                <label for="block">BLOCK</label>
+            </th>
+            <td>
+                <input class="input is-small" type="text" name="block" id="block">
+            </td>
+        </tr>
+
+        <tr>
+            <th>
+                <label for="taxi">TAXI</label>
+            </th>
+            <td>
+                <input class="input is-small" type="text" name="taxi" id="taxi">
+            </td>
+        </tr>
+
+        <tr>
+            <th>
+                <label for="trip">TRIP</label>
+            </th>
+            <td>
+                <input class="input is-small" type="text" name="trip" id="trip">
+            </td>
+        </tr>
+
+        <tr>
+            <th>
+                <label for="eet">EET:</label>
+            </th>
+            <td>
+                <input class="input is-small" type="text" name="eet" id="eet">
+            </td>
+        </tr>
+
+        <tr>
+            <th>
+                <label for="crew">CREW:</label>
+            </th>
+            <td>
+                <input class="input is-small" type="text" name="crew" id="crew">
+            </td>
+        </tr>
+
+        <tr>
+            <th>
+                <label for="dow">DOW:</label>
+            </th>
+            <td>
+                <input class="input is-small" type="text" name="dow" id="dow">
+            </td>
+        </tr>
+
+        <tr>
+            <th>
+                <label for="doi">DOI:</label>
+            </th>
+            <td>
+                <input class="input is-small" type="text" name="doi" id="doi">
+            </td>
+        </tr>
+    </tbody>
+</table>

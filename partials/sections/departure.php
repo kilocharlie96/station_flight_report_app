63e94e7fe96148557b55596f8605bb79546c2619
kilocharlie96 <cernak.kiko@gmail.com>
2024-07-18 12:21:09 +0200

    Arrival and departure structure changed.

--- partials/sections/departure.php
+++ partials/sections/departure.php
@@ -1,16 +1,18 @@
-<ul class="grid mt-6">
-    <li>
-        <label for="arr">DEPARTURE</label>
-        <input class="input" type="text" name="arr" id="arr" placeholder="IATA code">
-    </li>
-    <li>
-        <label for="">STD</label>
-        <input class="input" type="text" name="sta" id="sta">
-    </li>
-    <li>
-        <label for="">ATD</label>
-        <input class="input" type="text" name="ata" id="ata">
-    </li>
-</ul>
+<div class="fixed-grid has-3-cols mt-6">
+    <ul class="grid mt-6">
+        <li>
+            <label class="has-text-weight-bold" for="arr">DEPARTURE</label>
+            <input class="input" type="text" name="arr" id="arr" placeholder="IATA code">
+        </li>
+        <li>
+            <label for="">STD</label>
+            <input class="input" type="text" name="sta" id="sta">
+        </li>
+        <li>
+            <label for="">ATD</label>
+            <input class="input" type="text" name="ata" id="ata">
+        </li>
+    </ul>
 
-<?php include 'pax-and-cargo.php'; ?>
+    <?php include 'pax-and-cargo.php'; ?>
+</div>
